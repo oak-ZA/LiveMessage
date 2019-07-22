b6897db25a9da2d5816324670e1edb15bef8fe5f
张奥 <2515912396@qq.com>
2019-07-22 15:06:39 +0800

    first commit

---
+++ LiveMessage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiveMessage
+//
+//  Created by 张奥 on 2019/7/22.
+//  Copyright © 2019年 张奥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LiveMessage/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  LiveMessage
+//
+//  Created by 张奥 on 2019/7/22.
+//  Copyright © 2019年 张奥. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *name = @"张奥";
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
